71e85f53baf42e926d9d3c5db7a5c7987026dd09
ybkuroki <kuroki.job.pc@gmail.com>
2020-06-07 17:36:43 +0900

    Upgrade Spring Boot 2.2.0 to 2.3.0

--- db/initdb.d/springboot/3_dml.sql
+++ db/initdb.d/springboot/3_dml.sql
@@ -3,7 +3,7 @@
 insert into account_master(id,name,password) values (1,'test','$2a$10$ZMTnuNltXgEPsVZcDsqc1endcVC7L3u066JNC0v4Nfj5zl1nE6YcO');
 insert into authority_master(id,authority) values (1,'ROLE_USER');
 
-insert into account_master_authority_master(account_master_id,authorities_id) values (1,1);
+insert into account_master_authorities(account_master_id,authorities_id) values (1,1);
 
 insert into category_master(id,name) values (1,'技術書');
 insert into category_master(id,name) values (2,'小説');
