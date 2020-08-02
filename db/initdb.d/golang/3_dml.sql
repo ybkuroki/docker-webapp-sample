f2d2de62f888bb6620dec573f2be286dd5a771e4
ybkuroki <45133652+ybkuroki@users.noreply.github.com>
2020-08-02 15:31:25 +0900

    Add a account data for authentication

--- db/initdb.d/golang/3_dml.sql
+++ db/initdb.d/golang/3_dml.sql
@@ -2,3 +2,5 @@
 
 INSERT INTO "category_master" ("name") VALUES ('技術書'), ('雑誌'), ('小説');
 INSERT INTO "format_master" ("name") VALUES ('書籍'), ('電子書籍');
+INSERT INTO "authority_master" ("name") VALUES ('Admin');
+INSERT INTO "account_master" ("name","password","authority_id") VALUES ('test','$2a$10$2Yx7YqbDtqNExKJy94/Og..ainGsy.5PCbCQp6I87GX6.sTfRsV2u',1);
